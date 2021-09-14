a8e37f091a6df25e76adafd81639f9330b691a39
Bryan Kok <bryan.wyern1@gmail.com>
2021-09-14 20:57:39 +0800

    Hello World demo

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Akihabara.Framework;
+using Akihabara.Framework.Packet;
+using Akihabara.Framework.Port;
+
+namespace mediapipe_smoke_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string graphConfigText = @"
+input_stream: ""in""
+output_stream: ""out""
+node {
+  calculator: ""PassThroughCalculator""
+  input_stream: ""in""
+  output_stream: ""out1""
+}
+node {
+  calculator: ""PassThroughCalculator""
+  input_stream: ""out1""
+  output_stream: ""out""
+}
+";
+            Console.WriteLine("Hello World!");
+            // Akihabara.Framework.ProtoCalculator.CalculatorGraphConfig config = 
+            const string inputStream = "in";
+            const string outputStream = "out";
+            OutputStreamPoller<string> outputStreamPoller;
+            CalculatorGraph graph = new CalculatorGraph(graphConfigText);
+            outputStreamPoller = graph.AddOutputStreamPoller<string>(outputStream).Value();
+
+            Console.WriteLine("Successfully added Hello World graph!");
+
+            Status graphStartResult = graph.StartRun();
+            Console.WriteLine(graphStartResult);
+
+
+            int timestamp = System.Environment.TickCount & System.Int32.MaxValue;
+            var inputPacket = new StringPacket("Hello World", new Timestamp(timestamp));
+
+            StringPacket outputPacket = new StringPacket();
+            Status pushedInput = graph.AddPacketToInputStream(inputStream, inputPacket);
+
+            Console.WriteLine("Successfully pushed input packet!");
+
+            if (outputStreamPoller.Next(outputPacket))
+            {
+                Console.WriteLine($"{outputPacket.Get()}");
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Akihabara Smoke Testing
+
+Sample snippets of code which demonstrate features of [Akihabara](https://github.com/vignetteapp/Akihabara), a C# .NET bindings library
